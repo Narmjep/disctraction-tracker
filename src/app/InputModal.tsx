5f4d2394fdd10796af91cf70138177d7a357bf08
Narmjep <raphael.matousek@gmail.com>
2025-03-03 01:02:02 +0100

    icon and modal style

--- src/app/InputModal.tsx
+++ src/app/InputModal.tsx
@@ -18,10 +18,13 @@ function InputModal({ modalIsOpen, setModalIsOpen, onInputSubmitted }: InputModa
         content: {
             top: '50%',
             left: '50%',
+            width: '100%',
+            height: '100%',
             right: 'auto',
             bottom: 'auto',
             marginRight: '-50%',
-            transform: 'translate(-50%, -50%)'
+            transform: 'translate(-50%, -50%)',
+            background: "rgb(0,0,0,0.8)"
         }
     }
 
@@ -33,23 +36,18 @@ function InputModal({ modalIsOpen, setModalIsOpen, onInputSubmitted }: InputModa
         <Modal
             isOpen={modalIsOpen}
             style={modalStyles}
-            contentLabel="Adda new Counter"
+            contentLabel="Add a new Counter"
         >
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h3>Add Counter</h3>
+            <div className="container d-flex justify-content-center align-items-center vh-100">
+                <div className="row w-50">
+                    <div className="col-8">
+                        <input type="text" id="ModalTextInput" className="form-control bg-dark text-white" placeholder="Select a name..." />
                     </div>
-                </div>
-                <div className="row"> 
-                    <div className="col">
-                        <input type="text" id="ModalTextInput" className="form-control" placeholder="Counter Label" />
+                    <div className="col-auto">
+                        <button type="button" className="btn btn-primary ml-1" onClick={onSubmitButtonClicked}>Add</button>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col d-flex justify-content-end">
-                        <button type="button" className="btn btn-primary" onClick={onSubmitButtonClicked}>Add</button>
-                        <button type="button" className="btn btn-secondary"onClick={close}>Cancel</button>
+                    <div className="col-auto">
+                        <button type="button" className="btn btn-danger ml-1" onClick={close}>Cancel</button>
                     </div>
                 </div>
             </div>
